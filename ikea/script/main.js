e07e811450561673a4a3c2b5b5ffabea765e06c9
Клапатун Антон Владимирович <klapatun@zao-rim.ru>
2020-09-30 19:26:00 +0700

    Добавлен скрипт для работы с меню и header вызывается через скрипт

--- ikea/script/main.js
+++ ikea/script/main.js
@@ -1,10 +1,20 @@
-
+'use strict';
 //day 1
+
 
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
+const catalogList = document.querySelector('.catalog-list');
+const subcatalog = document.querySelector('.subcatalog');
+const subcatalogHeader = document.querySelector('.subcatalog-header');
+const  btnReturn = document.querySelector('.btn-return');
+
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
 
 const openMenu = () => {
     catalog.classList.add('open');
@@ -14,8 +24,24 @@ const openMenu = () => {
 const closeMenu = () => {
     catalog.classList.remove('open');
     overlay.classList.remove('active');
-}
+    closeSubMenu();
+};
+
+const openSubMenu = event => {
+    event.preventDefault();
+    const itemList = event.target.closest('.catalog-list__item');
+    if(itemList){
+        subcatalogHeader.innerHTML = itemList.innerHTML;
+        subcatalog.classList.add('subopen');
+    };
+};
+
+const closeSubMenu = () => {
+    subcatalog.classList.remove('subopen');
+};
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
+catalogList.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
